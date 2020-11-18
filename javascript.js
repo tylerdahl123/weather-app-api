1718da301451c85d3ede5a33323d6d317deab5fe
tylerdahl123 <dahlgren15@gmail.com>
2020-11-17 18:45:09 -0800

    got the uv index to console log out and managed to get the stuff to append on the page...still only for one city though

--- javascript.js
+++ javascript.js
@@ -20,7 +20,40 @@ function displayWeatherInfo(city) {
         url: queryURL,
         method: "GET",
     }).then(function (response) {
-        console.log(response.main.temp)//managed to get the temp of where i am.//
+        console.log(response)
+        
+        let lon = response.coord.lon;
+        console.log(lon)
+        let lat = response.coord.lat;
+        console.log(lat);
+        let queryUV = `https://api.openweathermap.org/data/2.5/uvi?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+       $.ajax({
+           url:queryUV,
+           method: "GET",
+       }).then(function (uvResponse) {
+           console.log(uvResponse);
+       })
+       
+       
+       
+       
+       
+       
+       
+        let temperature = response.main.temp;
+        console.log(temperature)
+        let humidity = response.main.humidity;
+        console.log(response.main.humidity);
+
+
+        let cityDiv =$("<div class='city'>");
+        let header = $("<h4>").text(city);
+        let p1 = $("<p>").text("TEMP:" + temperature + "F");
+        let p2 = $("<p>").text("HUMIDITY: " + humidity + "%");
+        cityDiv.append(header,p1,p2);
+        $("#weather-view").empty();//empties the div of what was in it before 
+        $("#weather-view").prepend(cityDiv);
+        //managed to get the temp of where i am.//
     })}
 //makes the cities listed into buttons so that the user can click on them. 
 function renderButtons(city) {
@@ -34,6 +67,12 @@ $("#searchBtn").on("click", function (event) {
     event.preventDefault();
     let weather = $("#city-input").val();
     cities.push(weather);
+    renderButtons(weather);
     displayWeatherInfo(weather)
 });
 
+$(document).on("click", ".city-btn", function () {
+    let city = $(this).attr("data-name");
+    displayWeatherInfo(city);
+    console.log(displayWeatherInfo())
+});
