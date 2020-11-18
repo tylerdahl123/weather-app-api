83ad770c8b056c95e873cf8e2f38724ee902614a
tylerdahl123 <dahlgren15@gmail.com>
2020-11-17 21:16:09 -0800

    managed to get the cities to log and display...my class names were all messed up among other things.

--- javascript.js
+++ javascript.js
@@ -13,14 +13,14 @@ cities.forEach(function (city, index, originalArr) {
 
 function displayWeatherInfo(city) {
     let queryURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=${apiKey}&units=imperial`;//learned this during the project...make the key and city a variable to the user puts it into the url and thats what it searches for.//
+    
 
-
-   
+   //call for the city and the information//
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
-        console.log(response)
+        // console.log(response)
         
         let lon = response.coord.lon;
         console.log(lon)
@@ -31,7 +31,7 @@ function displayWeatherInfo(city) {
            url:queryUV,
            method: "GET",
        }).then(function (uvResponse) {
-           console.log(uvResponse);
+        //    console.log(uvResponse);
            let temperature = response.main.temp;
            let windSpeed = response.wind.speed;
            let humidity = response.main.humidity
@@ -39,7 +39,7 @@ function displayWeatherInfo(city) {
            let header = $("<h4>").text(city);
            let p1 = $("<p>").text("Humidity: " + humidity + "%");
            let p2 =$("<p>").text("Wind Speed: " + windSpeed + "M.P.H")
-           let p3 = $("<p>").text("Temperature: " + temperature + "F")
+           let p3 = $("<p>").text("Temperature: " + temperature + " F")
            let p4 = $("<p>").text("UV Index: " + uvResponse.value);
 cityDiv.append(header,p1,p2,p3,p4);
 
@@ -47,31 +47,37 @@ cityDiv.append(header,p1,p2,p3,p4);
             $("#weather-view").prepend(cityDiv);
        })
     })
+  
+
 }
-       
-       
-       
-       
-       
-       
-//makes the cities listed into buttons so that the user can click on them. 
+
+     
 function renderButtons(city) {
     let btn = $("<button>");
+    btn.addClass("city-btn");
     btn.attr("data-name", city);
     btn.text(city);
     $(".cities-array").append(btn);
 }
+  
+       
+
+//makes the cities listed into buttons so that the user can click on them. 
+
 //makes the search button do things//
 $("#searchBtn").on("click", function (event) {
     event.preventDefault();
     let weather = $("#city-input").val();
     cities.push(weather);
+    localStorage.setItem("weather", JSON.stringify(cities))
     renderButtons(weather);
     displayWeatherInfo(weather)
+    
 });
-
+//controls what happens when you click the city buttons. 
 $(document).on("click", ".city-btn", function () {
     let city = $(this).attr("data-name");
     displayWeatherInfo(city);
     console.log(displayWeatherInfo())
+   
 });
